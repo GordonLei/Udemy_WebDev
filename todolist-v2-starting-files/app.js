782d39cf8c5044237d3fbe39d1758c54fa697e7a
Gordon Lei <gl1776@nyu.edu>
2020-12-25 13:07:41 -0500

    working on todolist v2 version. can add/delete items on list

---
+++ todolist-v2-starting-files/app.js
@@ -0,0 +1,107 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+//const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+  name: String
+}
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todo-list"
+})
+
+const item2 = new Item({
+  name: "item2"
+})
+
+const item3 = new Item({
+  name: "item3"
+})
+
+const defaultItems = [item1, item2,item3];
+
+
+
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems){
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if (err){
+          console.log("ERROR");
+        }
+        else{
+          console.log("Added default items to DB");
+        }
+      });
+      res.redirect("/");
+    }
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+  })
+  /*
+const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
+*/
+});
+
+app.post("/", function(req, res){
+
+  //const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+  res.redirect("/");
+  /*
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+  */
+});
+
+app.post("/delete", function(req, res){
+  //console.log(req.body.checkbox);
+  
+  const checkedItemId = req.body.checkbox;
+  Item.deleteOne({_id: checkedItemId}, function(err){
+    if (!err) {
+      console.log("Successfully deleted the item.");
+      res.redirect("/");
+    }
+  });
+  
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
